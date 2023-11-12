354630f393604f2036fcf4c038cd7eef9b923fa2
kriander <krisclarkston@gmail.com>
2023-11-11 20:23:11 -0700

    Initial python files

---
+++ chapter 5.py
@@ -0,0 +1,62 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+age == 8
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    age == 8
+NameError: name 'age' is not defined
+age = 8 elif age == 8 print('why did the hippo cry')
+SyntaxError: invalid syntax
+age = 8
+if age == 8 print ('Todd')
+SyntaxError: invalid syntax
+if age ==8:
+    print('Todd')
+
+    
+Todd
+if age == 6:
+    print('Kris')
+elif age == 8:
+    print('Todd')
+
+    
+Todd
+>>> age = 6
+>>> if age == 6:
+...     print('Kris')
+... elif age == 8:
+...     print('Todd')
+... 
+...     
+Kris
+>>> age = 8
+>>> if age == 6:
+...     print('Kris')
+... elif age == 8:
+...     print('Todd')
+... 
+...     
+Todd
+>>> age = 8
+>>> if age == 9:
+...     print('I saw a snake')
+...     elif age =  8:
+...         
+SyntaxError: invalid syntax
+>>> age = 8
+>>> if age == 9:
+...     print('I saw a snake')
+... elif age == 8:
+...     print('I saw a rabbit')
+... 
+...     
+I saw a rabbit
+>>> age = 9
+>>> if age == 9:
+...     print('I saw a snake')
+... elif age == 8:
+...     print('I saw a rabbit')
+... 
+...     
+I saw a snake
---
+++ chapter6.py
@@ -0,0 +1,8 @@
+age = 6
+print(age)
+
+if age == 6:
+    print('Kris')
+    age = 9
+    if age == 9: print('hack')
+    elif age == 10: print('yes')
